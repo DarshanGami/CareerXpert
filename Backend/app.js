bfdc386a120daca073f353cc8720c7fc5523f685
Rudra Patel <rudrapatel5917@gmail.com>
2024-11-09 04:01:48 +0530

    set up server, database and errorHandler

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connection } from './DB_connect.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true,
+  }));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+connection();
+app.use(errorHandler);
+
+export default app;
