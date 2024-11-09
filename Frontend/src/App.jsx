8bbcd9eced05c14c34d7e45a5d7495c32d57e9a9
unknown <ajaychovatiya307@gmail.com>
2024-11-10 01:39:52 +0530

    Add Login, SignUp & Avatar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/shared/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+]) 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      <RouterProvider router={appRouter}/>
     </>
   )
 }
--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -2,8 +2,12 @@ import { Popover, PopoverContent } from "@radix-ui/react-popover";
 import React from "react";
 import { PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
+import { AvatarImage } from "../ui/avatar";
+import { Avatar } from "@radix-ui/react-avatar";
+import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -11,19 +15,70 @@ const Navbar = () => {
           Job<span className="text-[#F83002]">Portal</span>
         </h1>
 
-        <div>
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="w-10 h-10 rounded-full cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                    className="object-cover rounded-full"
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80 p-4">
+                <div className="flex items-center gap-4 ">
+                  <Avatar className="w-10 h-10 rounded-full">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                      className="object-cover rounded-full"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Ajay Chovatiya</h4>
+                    <p className="text-sm text-muted-foreground">
+                      lorem ipsum dolor site
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
 
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button variant="outline">Open popover</Button>
-            </PopoverTrigger>
-            <PopoverContent></PopoverContent>
-          </Popover>
+                    <Button
+                      variant="link"
+                      className="px-2 py-1 rounded-lg border-none outline-none focus:outline-none focus:ring-0 focus:border-none"
+                      style={{ border: "none", boxShadow: "none" }}
+                    >
+                      view profile
+                    </Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button
+                      variant="link"
+                      className="px-2 py-1 rounded-lg border-none outline-none focus:outline-none focus:ring-0 focus:border-none"
+                      style={{ border: "none", boxShadow: "none" }}
+                    >
+                      Logout
+                    </Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
